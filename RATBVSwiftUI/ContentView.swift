d21c004c1e5854588f6274c52386a5ba1fad31f8
Sergiu Cosmin <warborg@gmail.com>
2020-06-03 00:36:52 +0300

    Initial Commit

---
+++ RATBVSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RATBVSwiftUI
+//
+//  Created by Sergiu Cosmin on 03/06/2020.
+//  Copyright © 2020 Sergiu Cosmin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
