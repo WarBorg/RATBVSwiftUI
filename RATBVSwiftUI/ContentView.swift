34ed3801a40b952f84ede13b0991615dad20ccec
Sergiu Cosmin <warborg@gmail.com>
2020-06-08 23:19:24 +0300

    Added simple listview design for bus lines

--- RATBVSwiftUI/ContentView.swift
+++ RATBVSwiftUI/ContentView.swift
@@ -10,9 +10,65 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack(alignment: .trailing) {
+                Text("Updated on 6/8/2020 21:38")
+                    .padding(.trailing)
+                    .font(.headline)
+                List {
+                    ForEach(0..<15) { item in
+                        VStack(alignment: .leading) {
+                            Text("Line \(item)")
+                                .font(.title)
+                                .fontWeight(.medium)
+                                .lineLimit(1)
+                                .multilineTextAlignment(.leading)
+                            Text("On the other hand, we denounce with righteous indignation and dislike men who cdscs cdscs  cdscsd cdscsdc")
+                                .font(.body)
+                                .fontWeight(.light)
+                                .lineLimit(2)
+                                .multilineTextAlignment(.leading)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Bus Lines"), displayMode:
+            .inline)
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
